1b980fbaff432bec7b2e9006ebbfec784b5d66f6
chagassantosmaria-cyber <chagas.santos.maria@escola.pr.gov.br>
2026-03-12 08:33:41 -0300

    Add files via upload

---
+++ JAVASCRIPT-MARIA EDUARDA/teste.js/String.js
@@ -0,0 +1,9 @@
+//Exemplos de texto
+let nome = "Maria Eduarda" 
+let sobreNome = "Chagas"  
+let nomeCompleto = nome + " " + sobreNome 
+//Concatenção das variáveis (+)
+console.log(nomeCompleto)
+let senha = "Senha1234"
+let stringDeNumeros = "123456789"
+console.log(stringDeNumeros + 2)
---
+++ JAVASCRIPT-MARIA EDUARDA/type/type_number.js
@@ -0,0 +1,7 @@
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+console.log(primeiroNumero + segundoNumero);
+
+console.log(somaDosValores);
